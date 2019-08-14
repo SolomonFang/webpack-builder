fc9b896aaf0c6e80814c00649dded92ef71a0759
方强 <qiang.fang@huilianyi.com>
2019-08-14 17:38:39 +0800

    fix(webpack-base-test): update file path

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,7 +6,7 @@ describe('webpack.base.js test case', () => {
     console.log(baseConfig)
 
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/fangqiang/Desktop/chris-learning/webpack-start/builder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/fangqiang/Desktop/chris-learning/webpack-start/builder-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('test/smoke/template/src/search/index.js') > -1, true);
     });
 });
